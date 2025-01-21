ce7346509a4190504bbd34738ed35daea2188bc6
EternalGerms <eternalgermes@proton.me>
2025-01-21 08:35:28 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("homepage");
+
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+
+app.listen(3000);
