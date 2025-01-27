54410e0a880e02aeb62bb0d4303003bf7fb2bb32
EternalGerms <eternalgermes@proton.me>
2025-01-27 16:47:26 -0300

    finished authentication part

--- server.js
+++ server.js
@@ -1,103 +1,177 @@
-require("dotenv").config()
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 const express = require("express");
-const db = require("better-sqlite3")("ourApp.db")
-db.pragma("journal_mode = WAL")
+const db = require("better-sqlite3")("ourApp.db");
+db.pragma("journal_mode = WAL");
 
 const createTables = db.transaction(() => {
-    db.prepare(`
+  db.prepare(
+    `
         CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username STRING NOT NULL UNIQUE,
         password STRING NOT NULL
         )
-        `).run()
-})
-createTables()
+        `
+  ).run();
+});
+createTables();
 
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use(function (req, res, next) {
-    res.locals.errors = []
+  res.locals.errors = [];
 
-    try {
-        const decoded = jwt.verify(req.cookies.ourSimpleApp, process.env.JWTSECRET)
-        req.user = decoded
-    } catch(err) {
-        req.user = false
-    }
+  try {
+    const decoded = jwt.verify(req.cookies.ourSimpleApp, process.env.JWTSECRET);
+    req.user = decoded;
+  } catch (err) {
+    req.user = false;
+  }
 
-    res.locals.user = req.user
-    console.log(req.user);
+  res.locals.user = req.user;
+  console.log(req.user);
 
-    next()
-})
+  next();
+});
 
 app.get("/", (req, res) => {
-    if(req.user)
-    {
-        return res.render("dashboard")
-    } 
-    res.render("homepage");
-
-})
+  if (req.user) {
+    return res.render("dashboard");
+  }
+  res.render("homepage");
+});
 
 app.get("/login", (req, res) => {
-    res.render("login")
-})
+  res.render("login");
+});
 
 app.get("/logout", (req, res) => {
-    res.clearCookie("ourSimpleApp")
-    res.redirect("/")
-})
+  res.clearCookie("ourSimpleApp");
+  res.redirect("/");
+});
+
+app.post("/login", (req, res) => {
+  let errors = [];
+
+  if (typeof req.body.username !== "string") req.body.username = "";
+  if (typeof req.body.password !== "string") req.body.password = "";
+
+  if (req.body.username.trim() == "") errors = ["Invalid username / password."];
+  if (req.body.password == "") errors = ["Invalid username / password."];
+
+  if (errors.length) {
+    return res.render("login", { errors });
+  }
+
+  const userInQuestionStatement = db.prepare(
+    "SELECT * FROM users WHERE USERNAME = ?"
+  );
+  const userInQuestion = userInQuestionStatement.get(req.body.username);
+
+  if (!userInQuestion) {
+    errors = ["Invalid username / password."];
+    return res.render("login", { errors });
+  }
+
+  const matchOrNot = bcrypt.compareSync(
+    req.body.password,
+    userInQuestion.password
+  );
+  if (!matchOrNot) {
+    errors = ["Invalid username / password."];
+    return res.render("login", { errors });
+  }
+
+  const ourTokenValue = jwt.sign(
+    {
+      exp: Math.floor(Date.now() / 1000 + 60 * 60 * 24),
+      userid: userInQuestion.id,
+      username: userInQuestion.username,
+    },
+    process.env.JWTSECRET
+  );
+
+  res.cookie("ourSimpleApp", ourTokenValue, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 1000 * 60 * 60 * 24,
+  });
+
+  res.redirect("/");
+});
+
+app.get("/create-post", (req, res) => {
+  res.render("/create-post");
+});
 
 app.post("/register", (req, res) => {
-    const errors = []
-    if (typeof req.body.username !== "string") req.body.username = ""
-    if (typeof req.body.password !== "string") req.body.password = ""
+  const errors = [];
+  if (typeof req.body.username !== "string") req.body.username = "";
+  if (typeof req.body.password !== "string") req.body.password = "";
 
-    req.body.username = req.body.username.trim()
+  req.body.username = req.body.username.trim();
 
-    if (!req.body.username) errors.push("You must provide a username")
-    if (req.body.username && req.body.username.length < 3) errors.push("Username must be at least 3 characters.")
-    if (req.body.username && req.body.username.length > 10) errors.push("Username cannot exceed 10 characters.")
-    if (req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/)) errors.push("Username can only contain letters and numbers.")
-    
-    if (!req.body.password) errors.push("You must provide a password.")
-    if (req.body.password && req.body.password.length < 3) errors.push("Password must be at least 3 characters.")
-    if (req.body.password && req.body.password.length > 10) errors.push("Password cannot exceed 10 characters.")
+  if (!req.body.username) errors.push("You must provide a username");
+  if (req.body.username && req.body.username.length < 3)
+    errors.push("Username must be at least 3 characters.");
+  if (req.body.username && req.body.username.length > 10)
+    errors.push("Username cannot exceed 10 characters.");
+  if (req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/))
+    errors.push("Username can only contain letters and numbers.");
 
-    if (errors.length) {
-        return res.render("homepage", {errors})
-    }
-    const salt = bcrypt.genSaltSync(10)
-    req.body.password = bcrypt.hashSync(req.body.password, salt)
+  const usernameStatement = db.prepare(
+    "SELECT * FROM users WHERE username = ?"
+  );
+  const usernameCheck = usernameStatement.get(req.body.username);
 
-    const ourStatement = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)")
-    const result = ourStatement.run(req.body.username, req.body.password)
+  if (usernameCheck) errors.push("Username already taken.");
 
-    const lookupStatement = db.prepare("SELECT * FROM users WHERE ROWID = ?");
-    const ourUser = lookupStatement.get(result.lastInsertRowid)
+  if (!req.body.password) errors.push("You must provide a password.");
+  if (req.body.password && req.body.password.length < 3)
+    errors.push("Password must be at least 3 characters.");
+  if (req.body.password && req.body.password.length > 10)
+    errors.push("Password cannot exceed 10 characters.");
 
-    const ourTokenValue = jwt.sign({exp: Math.floor(Date.now() / 1000 + 60 * 60 * 24),userid: ourUser.id, username: ourUser.username}, process.env.JWTSECRET)
+  if (errors.length) {
+    return res.render("homepage", { errors });
+  }
+  const salt = bcrypt.genSaltSync(10);
+  req.body.password = bcrypt.hashSync(req.body.password, salt);
 
-    res.cookie("ourSimpleApp", ourTokenValue, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "strict",
-        maxAge: 1000 * 60 * 60 * 24
-    })
+  const ourStatement = db.prepare(
+    "INSERT INTO users (username, password) VALUES (?, ?)"
+  );
+  const result = ourStatement.run(req.body.username, req.body.password);
 
-    res.send("Thanks!")
-    
-    
-})
+  const lookupStatement = db.prepare("SELECT * FROM users WHERE ROWID = ?");
+  const ourUser = lookupStatement.get(result.lastInsertRowid);
+
+  const ourTokenValue = jwt.sign(
+    {
+      exp: Math.floor(Date.now() / 1000 + 60 * 60 * 24),
+      userid: ourUser.id,
+      username: ourUser.username,
+    },
+    process.env.JWTSECRET
+  );
+
+  res.cookie("ourSimpleApp", ourTokenValue, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 1000 * 60 * 60 * 24,
+  });
+
+  res.redirect("/");
+});
 
 app.listen(3000);
