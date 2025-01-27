bed19be72f617678ecb706ae4d8d57cfba9f5f63
EternalGerms <eternalgermes@proton.me>
2025-01-27 00:50:21 -0300

    adding database and authentication

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
+const bcrypt = require("bcrypt")
 const express = require("express");
+const db = require("better-sqlite3")("ourApp.db")
+db.pragma("journal_mode = WAL")
+
+const createTables = db.transaction(() => {
+    db.prepare(`
+        CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username STRING NOT NULL UNIQUE,
+        password STRING NOT NULL
+        )
+        `).run()
+})
+createTables()
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -31,11 +46,27 @@ app.post("/register", (req, res) => {
     if (req.body.username && req.body.username.length > 10) errors.push("Username cannot exceed 10 characters.")
     if (req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/)) errors.push("Username can only contain letters and numbers.")
     
+    if (!req.body.password) errors.push("You must provide a password.")
+    if (req.body.password && req.body.password.length < 3) errors.push("Password must be at least 3 characters.")
+    if (req.body.password && req.body.password.length > 10) errors.push("Password cannot exceed 10 characters.")
+
     if (errors.length) {
         return res.render("homepage", {errors})
-    } else {
-        res.send("Now you are part of the club!");
     }
+    const salt = bcrypt.genSaltSync(10)
+    req.body.password = bcrypt.hashSync(req.body.password, salt)
+
+    const ourStatement = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)")
+    ourStatement.run(req.body.username, req.body.password)
+
+    res.cookie("ourSimpleApp", "supertopsecretvalue", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 1000 * 60 * 60 * 24
+    })
+
+    res.send("Thanks!")
     
     
 })
